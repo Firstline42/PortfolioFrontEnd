606dd71e595a216703f65f9c2c7bf875f31ede11
Firstline42 <numerabis@outlook.com>
2022-10-27 03:40:26 -0300

    Mi primer commit

---
+++ Angular/AppTaskListMod-III/src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para cenar",
+        day: "Agosto 5 a las 17:00",
+        reminder: true    
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: false
+    },
+   
+]

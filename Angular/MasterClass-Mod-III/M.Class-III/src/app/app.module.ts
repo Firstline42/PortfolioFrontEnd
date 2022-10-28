1dc4d142356b2b6b17baad9125bb260ad3682437
Firstline42 <numerabis@outlook.com>
2022-10-28 01:07:43 -0300

    ampliando componentes

--- Angular/MasterClass-Mod-III/M.Class-III/src/app/app.module.ts
+++ Angular/MasterClass-Mod-III/M.Class-III/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DivgeneralComponent } from './components/divgeneral/divgeneral.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DivgeneralComponent
+    DivgeneralComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

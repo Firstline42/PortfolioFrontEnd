9beb697d2e7ad62f99bba790199eb7b3a2bacbb4
Firstline42 <numerabis@outlook.com>
2022-11-27 20:25:51 -0300

    agregando servicios

--- Angular/Portfolio/src/app/components/divgeneral/divgeneral.component.ts
+++ Angular/Portfolio/src/app/components/divgeneral/divgeneral.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-divgeneral',
-  templateUrl: './divgeneral.component.html',
-  styleUrls: ['./divgeneral.component.css']
+  templateUrl: './divgeneral.component.html',         
+  styleUrls: ['./divgeneral.component.css']           
 })
 export class DivgeneralComponent implements OnInit {
 
-  constructor(private datosPortfolio:PortfolioService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos();
   }
 
 }
